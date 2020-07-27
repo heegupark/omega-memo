e01109fbc2d9b791611973abd62c17e4efd43913
heegupark <slhead@gmail.com>
2020-07-27 08:46:47 -0700

    fixed disclaimer

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -70,7 +70,7 @@ export default function Disclaimer(props) {
             <p className="mx-auto my-2 h5">Welcome to Omega Memo</p>
             <div className="mx-3 my-2 text-center">This app is created strictly for demonstration purposes. By clicking the button below, you accept that Omega Memo do not guarantee storing the memos that you write.</div>
             <button className="btn btn-sm btn-danger mx-auto my-2" onClick={handleAcceptClick}>Accept</button>
-            <p className="mx-auto my-2 text-warning text-center">This application is in-progress. I built this app using React, Node.js, Material UI, socker.io, and MongoDB to provide a function for users to play with memos.</p>
+            <p className="mx-auto my-2 text-warning text-center">This application is in-progress. I built this app using React, Node.js, Material UI, socket.io, and MongoDB to provide a function for users to play with memos.</p>
             <div className="mx-3 my-2 text-center text-secondary">{'If you have any questions, please email to '}<a href="mailto:omegathrone@omegathrone.com">omegathrone@omegathrone.com</a>.</div>
           </div>
         </Fade>
